731d9e26cb21d2f3b21e5c632b6cfe33be0171f6
hrushikeshjena9 <sonuhrushikeshshk.9999@gmail.com>
2025-01-13 18:25:29 +0530

    Update OurCourses.jsx

--- src/home-sections/OurCourses.jsx
+++ src/home-sections/OurCourses.jsx
@@ -1,3 +1,149 @@
+// import RightArrow1 from "../assets/arrow-right1.png";
+// import RightArrow from "../assets/arrow-right.png";
+// import FilterImg from "../assets/filter.png";
+// import Communication from "../assets/4.png";
+// import Professional from "../assets/0.png";
+// import Front from "../assets/5.png";
+// import Sales from "../assets/3.png";
+// import Territory from "../assets/2.png";
+// import Retail from "../assets/1.png";
+// import { Link } from "react-router-dom";
+// import { FiArrowRight } from "react-icons/fi";
+
+// function OurCourses() {
+//   return (
+//     <>
+//       <div className="courses">
+//         <section className=" container mx-auto p-4 z-10">
+//           <div className="mx-8">
+//             <div className="text-center mb-6">
+//               <h2 className="text-3xl uppercase md:text-2xl font-bold bg-gradient-to-r from-[#DB0032] to-[#FA6602] text-transparent bg-clip-text">
+//                 Our Courses
+//               </h2>
+//             </div>
+
+//             <div className="grid grid-cols-2  text-white gap-4 lg:gap-4 xl:gap-4 2xl:gap-12 md:grid-cols-3 lg:grid-cols-6 mb-6">
+//               <div className="card p-2  shadow-lg">
+//                 <div className="w-[112.19px] h-[112.19px] rounded-full bg-aliceblue mx-auto mb-4">
+//                   <img src={Professional} />
+//                 </div>
+//                 <p className="text-center text-sm md:text-[12px] px-1 uppercase">
+//                   Professional Selling Skills
+//                 </p>
+//               </div>
+
+//               <div className="card pt-2 shadow-lg">
+//                 <div className="w-[112.19px] h-[112.19px] rounded-full bg-aliceblue mx-auto  mb-4">
+//                   <img src={Communication} />
+//                 </div>
+//                 <p className="text-center px-3  text-sm md:text-[12px] uppercase ">
+//                   Communication Skills for Sales Staff
+//                 </p>
+//               </div>
+
+//               <div className="card p-2 shadow-lg">
+//                 <div className="w-[112.19px] h-[112.19px] rounded-full bg-aliceblue mx-auto mb-4">
+//                   <img src={Front} />
+//                 </div>
+//                 <p className="text-center text-sm md:text-[12px] px-1 uppercase">
+//                   Front Line Retail Selling Skills
+//                 </p>
+//               </div>
+
+//               <div className="card p-2 shadow-lg">
+//                 <div className="w-[112.19px] h-[112.19px] rounded-full bg-aliceblue mx-auto mb-4">
+//                   <img src={Sales} />
+//                 </div>
+//                 <p className="text-center text-sm md:text-[12px] px-3 uppercase">
+//                   Sales Management
+//                 </p>
+//               </div>
+
+//               <div className="card p-2 shadow-lg">
+//                 <div className="w-[112.19px] h-[112.19px] rounded-full bg-aliceblue mx-auto mb-4">
+//                   <img src={Territory} />
+//                 </div>
+//                 <p className="text-center text-sm md:text-[12px]  uppercase">
+//                   Sales Territory Planning & Routing
+//                 </p>
+//               </div>
+
+//               <div className="card p-2 shadow-lg">
+//                 <div className="w-[112.19px] h-[112.19px] rounded-full bg-aliceblue mx-auto mb-4">
+//                   <img src={Retail} />
+//                 </div>
+//                 <p className="text-center text-sm md:text-[12px] px-1 uppercase">
+//                   Retail Sales Planning and Forecasting
+//                 </p>
+//               </div>
+//             </div>
+
+//             <div className="flex flex-col md:flex-wrap lg:flex-wrap xl:flex-row lg:m xl:mx-7  justify-between items-center gap-4">
+//               {/* <Link
+//                 to="schedule-a-consultation"
+//                 type="button"
+//                 className="text-white group   transition-all duration-300 ease-out  transform   uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none sm:text-sm text-xs md:text-[14px] lg:text-[14px] xl:text-[16px] px-5 py-2.5 w-full md:py-4 sm:w-[378px] lg:px-3 lg:py-3  xl:px-6 xl:py-3 md:w-[380px] xl:w-auto lg:w-[380px] flex items-center justify-center"
+//               >
+//                 <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white  group-hover:w-full group-hover:h-full opacity-10"></span>
+//                 Schedule a Consultation
+//                 <img src={RightArrow1} alt="Right Arrow1" className="w-6 h-6" />
+//               </Link> */}
+
+//               <button
+//                 type="button"
+//                 className="relative  font-medium overflow-hidden group  text-white group   transition-all duration-300 ease-out  transform   uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none sm:text-sm text-xs md:text-[14px] lg:text-[14px] xl:text-[16px] px-5 py-2.5 w-full md:py-4 sm:w-[378px] lg:px-3 lg:py-3  xl:px-6 xl:py-3 md:w-[380px] xl:w-auto lg:w-[380px] flex items-center justify-center"
+//               >
+//                 <span className="absolute inset-0 w-0 h-full bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+//                 <span className="relative text-white group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#DB0032] to-[#FA6602] flex items-center">
+//                   Schedule a Consultation
+//                   <img
+//                     src={RightArrow1}
+//                     alt="Arrow Icon"
+//                     className="w-6 h-6 ml-2 transition-transform duration-300 ease-in-out"
+//                   />
+//                   {/* <FiArrowRight className="hidden w-6 h-6 ml-2 text-[#FA6602] transition-opacity duration-300 ease-in-out group-hover:inline-block group-hover:opacity-100" /> */}
+//                 </span>
+//               </button>
+//               <div className="flex justify-between items-center border-2">
+//                 <img
+//                   src={FilterImg}
+//                   alt="Filter Icon"
+//                   className="w-6 h-6 ml-2"
+//                 />
+//                 <input
+//                   type="text"
+//                   placeholder="Search Courses"
+//                   className="flex-1 w-full  px-6 py-0 sm:px-4 sm:py-1 md:px-4 md:py-1 lg:px-4 lg:py-1 xl:px-4 xl:py-1 2xl:px-4 2xl:py-1  font-bold placeholder:text-sm uppercase outline-none"
+//                 />
+//                 <Link
+//                   to="book-now"
+//                   type="button"
+//                   className="text-white group transition-transform duration-500 ease-out transform   w-auto  uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none text-[12px]  sm:text-sm md:text-[14px] px-5 py-2.5 sm:w-auto lg:w-auto xl:w-auto md:px-6 md:py-3 md:w-auto flex items-center justify-center"
+//                 >
+//                   <span className="absolute inset-0 w-0 h-full bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+//                   <span className="relative text-white group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#DB0032] to-[#FA6602] flex items-center">
+//                   Book Now</span>
+//                 </Link>
+//               </div>
+
+//               <Link
+//                 to="discover-our-courses"
+//                 type="button"
+//                 className="text-[#000] transition-transform duration-500 ease-out transform  flex md:px-6 md:py-2 lg:py-2.5 xl:py-2.5 2xl:py-2.5 sm:w-[378px] md:w-[385px] justify-center  w-full xl:w-auto lg:w-[385px]  uppercase gap-3 sm:justify-center md:justify-center xl:justify-between lg:justify-center  space-x-2 items-center font-bold text-sm px-4 py-2 hover:text-[#000]  border-[#000] border-btn2"
+//               >
+//                 Discover Our Courses
+//                 <img src={RightArrow} alt="Right Arrow" className="w-6 h-6" />
+//               </Link>
+//             </div>
+//           </div>
+//         </section>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default OurCourses;
+
 import RightArrow1 from "../assets/arrow-right1.png";
 import RightArrow from "../assets/arrow-right.png";
 import FilterImg from "../assets/filter.png";
@@ -89,9 +235,9 @@ function OurCourses() {
                 <img src={RightArrow1} alt="Right Arrow1" className="w-6 h-6" />
               </Link> */}
 
-              <button
+              {/* <button
                 type="button"
-                className="relative  font-medium overflow-hidden group  text-white group   transition-all duration-300 ease-out  transform   uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none sm:text-sm text-xs md:text-[14px] lg:text-[14px] xl:text-[16px] px-5 py-2.5 w-full md:py-4 sm:w-[378px] lg:px-3 lg:py-3  xl:px-6 xl:py-3 md:w-[380px] xl:w-auto lg:w-[380px] flex items-center justify-center"
+                className="relative hover:border-2 border-black  font-medium overflow-hidden group  text-white group   transition-all duration-300 ease-out  transform   uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none sm:text-sm text-xs md:text-[14px] lg:text-[14px] xl:text-[16px] px-5 py-2.5 w-full md:py-4 sm:w-[378px] lg:px-3 lg:py-3  xl:px-6 xl:py-3 md:w-[380px] xl:w-auto lg:w-[380px] flex items-center justify-center"
               >
                 <span className="absolute inset-0 w-0 h-full bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
                 <span className="relative text-white group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#DB0032] to-[#FA6602] flex items-center">
@@ -101,10 +247,25 @@ function OurCourses() {
                     alt="Arrow Icon"
                     className="w-6 h-6 ml-2 transition-transform duration-300 ease-in-out"
                   />
-                  {/* <FiArrowRight className="hidden w-6 h-6 ml-2 text-[#FA6602] transition-opacity duration-300 ease-in-out group-hover:inline-block group-hover:opacity-100" /> */}
+                  <FiArrowRight className="hidden w-6 h-6 ml-2 text-[#FA6602] transition-opacity duration-300 ease-in-out group-hover:inline-block group-hover:opacity-100" />
                 </span>
-              </button>
-              <div className="flex justify-between items-center border-2">
+              </button> */}
+              <button
+  type="button"
+  className="relative font-medium overflow-hidden group text-white transition-all duration-300 ease-out transform uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none sm:text-sm text-xs md:text-[14px] lg:text-[14px] xl:text-[16px] px-5 py-2.5 w-full md:py-4 sm:w-[378px] lg:px-3 lg:py-3 xl:px-6 xl:py-3 md:w-[380px] xl:w-auto lg:w-[380px] flex items-center justify-center"
+>
+  <span className="absolute inset-0 w-0 h-full   bg-white transition-all duration-300 ease-in-out group-hover:w-full group-hover:bg-gradient-to-tr group-hover:from-[#060b33] group-hover:to-[#383f71]"></span>
+  <span className="relative text-white group-hover:text-white flex items-center">
+    Schedule a Consultation
+    <img
+      src={RightArrow1}
+      alt="Arrow Icon"
+      className="w-6 h-6 ml-2 transition-transform duration-300 ease-in-out"
+    />
+  </span>
+</button>
+
+              <div className="flex justify-between items-center border-2 border-black">
                 <img
                   src={FilterImg}
                   alt="Filter Icon"
@@ -118,10 +279,10 @@ function OurCourses() {
                 <Link
                   to="book-now"
                   type="button"
-                  className="text-white group transition-transform duration-500 ease-out transform   w-auto  uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none text-[12px]  sm:text-sm md:text-[14px] px-5 py-2.5 sm:w-auto lg:w-auto xl:w-auto md:px-6 md:py-3 md:w-auto flex items-center justify-center"
+                  className="text-white   group transition-transform duration-500 ease-out transform   w-auto  uppercase bg-gradient-to-r from-[#DB0032] to-[#FA6602] hover:bg-gradient-to-bl focus:outline-none text-[12px]  sm:text-sm md:text-[14px] px-5 py-2.5 sm:w-auto lg:w-auto xl:w-auto md:px-6 md:py-3 md:w-auto flex items-center justify-center"
                 >
-                  <span className="absolute inset-0 w-0 h-full bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
-                  <span className="relative text-white group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#DB0032] to-[#FA6602] flex items-center">
+                  <span className="absolute inset-0 w-0 h-full   bg-white transition-all duration-300 ease-in-out group-hover:w-full group-hover:bg-gradient-to-tr group-hover:from-[#060b33] group-hover:to-[#383f71]"></span>
+                  <span className="relative text-white group-hover:text-white flex items-center">
                   Book Now</span>
                 </Link>
               </div>
