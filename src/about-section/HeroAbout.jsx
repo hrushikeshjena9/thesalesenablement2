d5c82aafd1ba30629434593c037cd05b87598381
Hrushikesh <sonuhrushikeshshk.9999@gmail.com>
2025-01-14 16:57:26 +0530

    new-ab

---
+++ src/about-section/HeroAbout.jsx
@@ -0,0 +1,31 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
+import Navbar from "../components/Navbar";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+function HeroAbout() {
+  return (
+    <>
+      <section>
+        <div className="relative w-full h-full overflow-hidden course-bg">
+          <div className="relative   bg-layer">
+            <Navbar />
+
+            <div className="text-white flex items-center justify-center container mx-auto px-4 h-[200px]">
+              <h1 className="text-5xl uppercase font-bold">About Us</h1>
+            </div>
+            <div className="text-white flex items-center justify-center gap-6 py-12">
+              <div className="uppercase">Home</div>
+              <span className=" bg-gradient-to-r from-[#DB0032] to-[#FA6602]">
+                <MdOutlineKeyboardArrowRight size={30} />
+              </span>
+
+              <div className="uppercase">About</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default HeroAbout;
