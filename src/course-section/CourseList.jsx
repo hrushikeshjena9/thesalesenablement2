5668a319fea9a84601d456e9ff40a84ce6c4c709
Hrushikesh <sonuhrushikeshshk.9999@gmail.com>
2025-01-25 14:26:28 +0530

    new

--- src/course-section/CourseList.jsx
+++ src/course-section/CourseList.jsx
@@ -18,10 +18,176 @@ const courses = [
     description:
       "Learn essential skills to close deals and build long-lasting relationships, For forward-thinking leaders who are ready to develop a more responsive and innovative culture.",
 
-    location: { text: "Onsite", icon: <BsBuildings /> },
+    location: { text: "On Site", icon: <BsBuildings /> },
     audience: "sales-teams",
-    topics: { sellingSkills: true },
+    topics: ["Selling Skills"],
     courseName: { text: "Courses", icon: <FaBook /> },
+  },
+  {
+    id: 2,
+    title: "Front Line Retail Selling Skills",
+    description:
+      "Enhance your retail sales skills with practical, real-world techniques, For forward-thinking leaders who are ready to develop a more responsive and innovative culture.",
+
+    location: { text: "Virtual", icon: <FaGlobeAmericas /> },
+    audience: "sales-leaders",
+    topics: ["frontlineRetail"],
+    courseName: { text: "Courses", icon: <FaBook /> },
+  },
+  {
+    id: 3,
+    title: "Sales Management",
+    description:
+      "Become an expert in managing sales teams and achieving sales targets, For forward-thinking leaders who are ready to develop a more responsive and innovative culture.",
+
+    location: { text: "On Site", icon: <BsBuildings /> },
+    audience: "sales-teams",
+    topics: ["salesManagement"],
+    courseName: { text: "Courses", icon: <FaBook /> },
+  },
+
+  {
+    id: 4,
+    title: "Communication Skills for Sales Staff",
+    description:
+      "Get the most out of your CRM system to close deals faster, For forward-thinking leaders who are ready to develop a more responsive and innovative culture.",
+
+    location: { text: "On Site", icon: <BsBuildings /> },
+    audience: "sales-teams",
+    topics: ["crm", "salesEnablement"],
+    courseName: { text: "Master Class", icon: <SiGoogleclassroom /> },
+  },
+
+  {
+    id: 5,
+    title: "Sales Territory Planning & Routing",
+    description:
+      "Streamline your sales processes for maximum efficiency and results, For forward-thinking leaders who are ready to develop a more responsive and innovative culture.",
+
+    audience: "sales-leaders",
+    topics: ["salesEnablement", "processOptimization"],
+    location: { text: "On Site", icon: <BsBuildings /> },
+    courseName: { text: "Master Class", icon: <SiGoogleclassroom /> },
+  },
+  {
+    id: 6,
+    title: "Retail Sales Planning and Forecasting",
+    description:
+      "Master the art of negotiation to close more deals successfully, For forward-thinking leaders who are ready to develop a more responsive and innovative culture.",
+
+    location: { text: "Virtual", icon: <FaGlobeAmericas /> },
+    audience: "sales-reps",
+    topics: ["salesEnablement", "negotiation"],
+    courseName: { text: "Courses", icon: <FaBook /> },
+  },
+  {
+    id: 7,
+    title: "Advanced Leadership Strategies",
+    description:
+      "Elevate your leadership skills to build high-performing teams and inspire excellence.",
+
+    location: { text: "On Site", icon: <FaChalkboardTeacher /> },
+    audience: "team-leaders",
+    topics: ["leadership", "teamBuilding"],
+    courseName: { text: "Workshops", icon: <FaUsers /> },
+  },
+  {
+    id: 8,
+    title: "Customer Relationship Management",
+    description:
+      "Learn effective CRM strategies to strengthen customer loyalty and drive sales growth.",
+
+    location: { text: "Hybrid", icon: <FaGlobeAmericas /> },
+    audience: "sales-managers",
+    topics: ["customerRetention", "relationshipManagement"],
+    courseName: { text: "Seminars", icon: <FaChalkboardTeacher /> },
+  },
+  {
+    id: 9,
+    title: "Digital Marketing Essentials",
+    description:
+      "Understand core digital marketing principles to expand your brand's online presence.",
+
+    location: { text: "Virtual", icon: <FaGlobeAmericas /> },
+    audience: "marketing-professionals",
+    topics: ["seo", "socialMedia"],
+    courseName: { text: "Courses", icon: <FaBook /> },
+  },
+  {
+    id: 10,
+    title: "Supply Chain Optimization",
+    description:
+      "Streamline operations to minimize costs and maximize efficiency across the supply chain.",
+
+    location: { text: "On Site", icon: <FaChalkboardTeacher /> },
+    audience: "supply-chain-managers",
+    topics: ["logistics", "processImprovement"],
+    courseName: { text: "Programs", icon: <FaUsers /> },
+  },
+  {
+    id: 11,
+    title: "Negotiation Skills for Professionals",
+    description:
+      "Enhance your negotiation skills to achieve better outcomes in business deals.",
+
+    location: { text: "Virtual", icon: <FaGlobeAmericas /> },
+    audience: "executives",
+    topics: ["negotiation", "salesStrategy"],
+    courseName: { text: "Master Class", icon: <FaBook /> },
+  },
+  {
+    id: 12,
+    title: "Financial Management Fundamentals",
+    description:
+      "Learn essential financial management practices to drive business growth effectively.",
+
+    location: { text: "On Site", icon: <FaChalkboardTeacher /> },
+    audience: "finance-managers",
+    topics: ["financialPlanning", "budgetManagement"],
+    courseName: { text: "Courses", icon: <FaBook /> },
+  },
+  {
+    id: 13,
+    title: "Innovation and Creativity in Business",
+    description:
+      "Foster innovation and creativity to develop breakthrough business solutions.",
+
+    location: { text: "Hybrid", icon: <FaGlobeAmericas /> },
+    audience: "business-leaders",
+    topics: ["innovation", "creativeThinking"],
+    courseName: { text: "Workshops", icon: <FaUsers /> },
+  },
+  {
+    id: 14,
+    title: "Project Management Excellence",
+    description:
+      "Master project management techniques to deliver projects on time and within budget.",
+
+    location: { text: "On Site", icon: <FaChalkboardTeacher /> },
+    audience: "project-managers",
+    topics: ["agile", "teamCollaboration"],
+    courseName: { text: "Programs", icon: <FaBook /> },
+  },
+  {
+    id: 15,
+    title: "Emotional Intelligence for Leaders",
+    description:
+      "Harness emotional intelligence to build stronger relationships and lead with empathy.",
+
+    location: { text: "Virtual", icon: <FaGlobeAmericas /> },
+    audience: "leaders",
+    topics: ["emotionalIntelligence", "leadership"],
+    courseName: { text: "Seminars", icon: <FaUsers /> },
+  },
+  {
+    id: 16,
+    title: "Strategic Thinking and Problem Solving",
+    description:
+      "Develop critical thinking and problem-solving skills to navigate complex business challenges.",
+    location: { text: "Hybrid", icon: <FaGlobeAmericas /> },
+    audience: "executives",
+    topics: ["strategy", "decisionMaking"],
+    courseName: { text: "Workshops", icon: <FaChalkboardTeacher /> },
   },
 ];
 
@@ -107,7 +273,6 @@ const CourseList = ({ filters }) => {
           </div>
         </div>
       ) : (
-
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           {filteredCourses.map((course) => (
             <CourseItem key={course.id} course={course} />
